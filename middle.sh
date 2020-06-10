60156d865a8baa2b6140e40515c376c98d9b5594
Mark Laufersweiler <laufers@ou.edu>
2020-06-10 10:24:30 -0500

    added comments about variable order for script

--- middle.sh
+++ middle.sh
@@ -1,4 +1,8 @@
 # Adding the ability to run on any file
 # added ability to set line numners
 
+# $1 is filename
+# $2 first set of lines in file
+# $3 end number of lines 
+
 head -n "$2" "$1"  | tail -n "$3"  
