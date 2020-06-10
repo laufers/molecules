7a6f26aa9a9e16e9abfe5246e1d5b5163adec7b6
Mark Laufersweiler <laufers@ou.edu>
2020-06-10 10:26:17 -0500

    added instruct on how to run

--- middle.sh
+++ middle.sh
@@ -1,6 +1,7 @@
 # Adding the ability to run on any file
 # added ability to set line numners
 
+# to execute: bash middle $1 $2 $3 where 
 # $1 is filename
 # $2 first set of lines in file
 # $3 end number of lines 
